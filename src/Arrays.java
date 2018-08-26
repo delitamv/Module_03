32e66b144fceb714b8c4dcd03071379e4ba46e72
Marina Delita <delita@ukr.net>
2018-08-26 16:02:51 +0300

    task 2

--- src/Arrays.java
+++ src/Arrays.java
@@ -16,7 +16,7 @@ public class Arrays {
         // найти минимальное число в массиве и вывести в консоль (без использования сортировки)
         int min = array[0];
         for (int i = 0; i < array.length; i++) {
-            if(array[i] < min){
+            if (array[i] < min) {
                 min = array[i];
             }
         }
@@ -24,8 +24,8 @@ public class Arrays {
 
         // найти максимальное число в массиве и вывести в консоль
         int max = array[0];
-        for(int i = 0; i < array.length; i++){
-            if(array[i] > max){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
                 max = array[i];
             }
         }
@@ -33,7 +33,7 @@ public class Arrays {
 
         // посчитать кол-во повторений числа 5 и вывести в консоль
         int counter = 0;
-        for(int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             if (array[i] == 5) {
                 counter += 1;
             }
@@ -41,31 +41,37 @@ public class Arrays {
         System.out.println("Counter: " + counter);
 
         // вывести в консоль отсортированный массив
-        for(int i=0; i < size; i++){
-            for(int j=1; j < (size-i); j++){
-                if(array[j-1] > array[j]){
+        for (int i = 0; i < size; i++) {
+            for (int j = 1; j < (size - i); j++) {
+                if (array[j - 1] > array[j]) {
                     //swap elements
-                    int temp = array[j-1];
-                    array[j-1] = array[j];
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
                     array[j] = temp;
                 }
 
             }
         }
-        for(int value : array){
-            System.out.print(value + " ");
+        // Вывод сортировки используя sort bubble
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
         }
-// вывести в консоль максимальное кол-во повторений чисел (пример 1)
-
-
-        for(int i = 0; i < array.length; i++){
-            int maxCounter = 0;
-            if(array[i]> 0){
-                int maxCounter ++;
-            }
+        // Задание 2 (дополнительное)
+        // вывести в консоль максимальное кол-во повторений чисел (пример 1)
+        int maxCount1 = 0;
+        int counter1 = 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    maxCount1 = array[i];
+                    counter1++;
+                }
+        }
+            System.out.println("Ответ: " + counter1 +". " + "Так как число " + maxCount1 + " " + "повторяется " + counter1 + " раза");
+            counter1 = 0;
+            break;
         }
     }
-
 }
 
 
